1f2b74660fa5861c34049de7d5c2edad3ae3287d
Ritesh Srivastava <16515667+riteshksriv@users.noreply.github.com>
2022-01-02 14:13:24 +0530

    Create saveB2ForiengAsset.py

---
+++ saveB2ForiengAsset.py
@@ -0,0 +1,124 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+from selenium.webdriver.support.ui import Select
+import time
+
+url = "http://localhost:50199"
+session_id = "10890368d927c544dad1e638e6d06012"
+
+driver = webdriver.Remote(command_executor=url,desired_capabilities={})
+driver.close()   # this prevents the dummy browser
+driver.session_id = session_id
+
+datas = [
+  #["type", "date","amount"]
+  ["RSU Shares","24-Oct-2020","123"]
+]
+#driver.maximize_window()
+
+def addData(element, data:str):
+  element.clear()
+  element.send_keys(data)
+
+
+def select(driver, data):
+  select = Select(driver.find_element_by_xpath('//*[@formcontrolname="DetailsFinancialInterest_CountryCode"]'))
+  select.select_by_value('2')
+  names = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_NameOfEntity']")
+  addData(names, 'Adobe Systems Inc')
+  nature = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_NatureOfEntity']")
+  addData(nature, 'Multi National Company')
+  address = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_AddressOfEntity']")
+  addData(address, '345 Park Ave, San Jose')
+  zips = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_ZipCode']")
+  addData(zips, '95110')
+
+  fillDate(driver, data[1])
+
+  select = Select(driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_NatureOfInt']"))
+  select.select_by_value('BENIFICIARY')
+  total = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_TotalInvestment']")
+  addData(total, data[2])
+  income = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_IncFromInt']")
+  addData(income, '0')
+  natureincome = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_NatureOfInc']")
+  addData(natureincome, data[0])
+  amount = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_IncTaxAmt']")
+  addData(amount, '0')
+  schedule = Select(driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_IncTaxSch']"))
+  schedule.select_by_value('NI')
+  item = driver.find_element_by_xpath("//*[@formcontrolname='DetailsFinancialInterest_IncTaxSchNo']")
+  addData(item, '0')
+  add = driver.find_element_by_xpath("//*[contains(@class, 'primaryButton') and contains(@class, 'largeButton') and contains(text(), 'Add')]")
+  add.click()
+  time.sleep(5)
+
+def clickBDetails(driver):
+  items = driver.find_elements_by_xpath("//*[contains(@class, 'rep_parent_comp')]")
+  for item in items:
+    if(item.text.startswith('B. Details of Financial Interest')):
+      item.click()
+      time.sleep(2)
+      break
+
+def clickAddAnother(driver):
+  items = driver.find_elements_by_xpath("//*[contains(@class, 'addDetailsButton') and contains(@class,'uniservenxtcmp_button') and contains(text(), 'Add Another')]")
+  for item in items:
+    parent = item.find_element_by_xpath("./../../../../../../..")
+    if(parent.text.startswith('B.')):
+      item.click()
+      time.sleep(10)
+      break
+
+def getviaparent(driver, parentxpath, xpath):
+  parent =  driver.find_element_by_xpath(parentxpath)
+  item = parent.find_element_by_xpath(xpath)
+  return item
+
+def clicktime(driver:WebDriver, xpath: str, since: str, add: int):
+  items = driver.find_elements_by_xpath("//*[contains(@class, '{xpath}') and not(contains(@class, '{xpath}s')) and not(contains(@class, 'old {xpath}'))]".format(xpath=xpath))
+  for item in items:
+    if((item.text.isdigit() and int(item.text)+add>=int(since)) or (item.text == since)):
+      item.click()
+      time.sleep(1)
+      break
+
+#
+def fillDate(driver:WebDriver, since:str):
+  parent =  driver.find_element_by_xpath("//*[contains(text(), 'Date since held')]")
+  icons = parent.find_elements_by_xpath("//*[contains(@class, 'input-group-text') and contains(@class, 'calender-icon')]")
+  for icon in icons:
+    if(icon.is_displayed()):
+      icon.click()
+      time.sleep(1)
+
+      button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.datepicker-days .datepicker-switch')))
+      button.click()
+      button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.datepicker-months .datepicker-switch')))
+      button.click()
+      button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.datepicker-years .datepicker-switch')))
+      button.click()
+
+      clicktime(driver, "decade", since[7:], 9)
+      clicktime(driver, "year", since[7:], 0)
+      clicktime(driver, "month", since[3:6], 0)
+      clicktime(driver, "day", since[0:2], 0)
+      break
+
+def fillAll(driver, data):
+  clickBDetails(driver)
+  clickAddAnother(driver)
+  select(driver, data)
+
+#fillDate(driver, "24-Apr-2016")
+for data in datas:
+  fillAll(driver, data)
+
+
+
